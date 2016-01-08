8fefd11168468fdc850646bb178462b07b6343d0
b-trea <wsz89425@gmail.com>
2016-01-08 04:24:32 +0000

    send list

--- myapp/routes/test.js
+++ myapp/routes/test.js
@@ -8,11 +8,7 @@ var connection = dbConnection.connection;
 router.get('/', function(req, res, next) {
 //  res.render('index', { title: 'Express' });
 
-	var strQuery = 'select board.boardNo as boardNo, board.title as title, \
-							board.insertId as insertId, board.insertDate as insertDate, \
-							user.name as writerName \
-					from board, user \
-					where board.insertId = user.userId';
+	var strQuery = 'select * from testTable;';
 	
 	var query = connection.query(strQuery,function(err,rows){
         console.log(rows);
