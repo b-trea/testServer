e9fc9cfc50520804e634a8088230c2e47900dfba
b-trea <wsz89425@gmail.com>
2016-01-21 11:22:19 +0900

    data 송수신 수정_2

--- myapp/routes/mypush.js
+++ myapp/routes/mypush.js
@@ -33,35 +33,25 @@ router.post('/register', function(req, res, next){
 	res.end();
 });
 
-router.get('/unregister', function(req, res, next){
+router.post('/unregister', function(req, res, next){
 	// DB Delete
 	console.log('[mypush] unregistering... ');
 
-	var body = '';
 
-	req.on('data', function(chunk){
-		body+=chunk;
-	});
+	var regId = req.body.regId;
+	var senderId = req.body.senderId;
 
-	req.on('end', function(){
-		var json = JSON.parse(body);
-		var regId = json.regId;
-		var senderId = json.senderId;
+	var strQuery = "delete from regId where regId=? and senderId=?;";
+	var insertData = [regId, senderId];
+	var query = connection.query(strQuery,insertData,function(err,rows){
+        if(err){
+        	console.error("delete error!");
+        	console.error(err);
+        	res.json(err);
+        }
 
-		var strQuery = "delete from regId where regId=? and senderId=?;";
-		var insertData = [regId, senderId];
-		var query = connection.query(strQuery,insertData,function(err,rows){
-	        if(err){
-	        	console.error("delete error!");
-	        	console.error(err);
-	        	res.json(err);
-	        }
-
-	        console.log('delete complete!');
-
-	        res.end();
-	    });
-	});
+        console.log('delete complete!');
+    });
 
 	res.end();
 });
@@ -70,55 +60,49 @@ router.get('/send', function(req, res, next){
 	// DB get regID and send message to regID
 	console.log('[mypush] Preparing for Send Message... ');
 
-	var body = '';
-	req.on('data', function(chunk){
-		body+=chunk;
-	});
 
-	req.on('end', function(){
-		var json = JSON.parse(body);
-		var senderId = json.senderId;
-		var key1 = 'keyqwe';
+	var senderId = req.body.senderId;
+	var key1 = 'keyqwe';
 
-		var message = new gcm.Message();
-		message.addData('title', 'TestMessage');
-		message.addData('key3', 'message Key3');
-		
-		var sender = new gcm.Sender(server_access_key);
-
-		var registrationIds = [];
-		var rows = '';
-
-		var strQuery = "select regId from regId where senderId='"+ senderId +"';";
+	var message = new gcm.Message();
+	message.addData('title', 'TestMessage');
+	message.addData('key3', 'message Key3');
 	
-		var query = connection.query(strQuery,function(err,rows){
+	var sender = new gcm.Sender(server_access_key);
 
-			if(err){
-				console.error("select error!");
-	        	console.error(err);	        	
-			}else{
-				for(var i in rows){
-					var rid = rows[i].regId;
-					rid = rid.replace(/(\s*)/g,"");
-					registrationIds.push(rid);
-				}
+	var registrationIds = [];
+	var rows = '';
 
-				console.log("Sending Message...");
+	var strQuery = "select regId from regId where senderId='"+ senderId +"';";
 
-				sender.send(message, registrationIds, 4, function(err, result){
-					if(err){
-						console.error('Sending message ERROR!');
-						console.error(err);
-					}else{
-						var r = result;
-						console.log('Send result : ' + r);
-					}
-				});
+	var query = connection.query(strQuery,function(err,rows){
 
-				console.log('send complete!!');
+		if(err){
+			console.error("select error!");
+        	console.error(err);	        	
+		}else{
+			for(var i in rows){
+				var rid = rows[i].regId;
+				rid = rid.replace(/(\s*)/g,"");
+				registrationIds.push(rid);
 			}
-	    });
-	});
+
+			console.log("Sending Message...");
+
+			sender.send(message, registrationIds, 4, function(err, result){
+				if(err){
+					console.error('Sending message ERROR!');
+					console.error(err);
+				}else{
+					var r = result;
+					console.log('Send result : ' + r);
+				}
+			});
+
+			console.log('send complete!!');
+		}
+    });
+
 
 	res.end();
 });
